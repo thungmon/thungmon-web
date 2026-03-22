1961c62b8cdaf40891e4251f4b1053b63e0ab73e
Dookdik <d.lalitlagsamanont@gmail.com>
2026-03-22 18:56:55 +0700

    fix: adjust footer padding in SiteLayout component

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -9,7 +9,7 @@ export default function SiteLayout({
     <div className="flex min-h-screen flex-col">
       <SiteNav />
       <div className="flex flex-1 flex-col">{children}</div>
-      <footer className="border-t border-black/6 bg-white py-6 text-center text-xs text-zinc-400">
+      <footer className="border-t border-black/6 bg-white py-4 text-center text-xs text-zinc-400">
         © thungmon 2026
       </footer>
     </div>
