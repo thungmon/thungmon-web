e596af013c819add67832ce3697988c5f59fff26
Dookdik <d.lalitlagsamanont@gmail.com>
2026-03-22 19:01:50 +0700

    fix: add background color to SiteLayout and ensure proper child element height

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -6,9 +6,9 @@ export default function SiteLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col bg-neutral-100">
       <SiteNav />
-      <div className="flex flex-1 flex-col">{children}</div>
+      <div className="flex h-full flex-1 flex-col">{children}</div>
       <footer className="border-t border-black/6 bg-white py-4 text-center text-xs text-zinc-400">
         © thungmon 2026
       </footer>
