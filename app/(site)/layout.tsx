845db212d99c72d6ab520c18eb3fdc77aa9e5179
Dookdik <d.lalitlagsamanont@gmail.com>
2026-03-22 18:51:40 +0700

    feat: add footer with copyright information to SiteLayout component

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -9,6 +9,9 @@ export default function SiteLayout({
     <>
       <SiteNav />
       {children}
+      <footer className="border-t border-black/6 bg-white py-6 text-center text-xs text-zinc-400">
+        © thungmon 2026
+      </footer>
     </>
   );
 }
